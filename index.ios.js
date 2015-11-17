10eda753e219be022b5719ce34f509241918accb
antigirl <qasimf@tcd.ie>
2015-11-17 21:49:33 +0000

    make graph requests, get token/refresh token

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,10 @@
 import React from 'react-native';
 import FBSDKLogin from 'react-native-fbsdklogin';
+import FBSDKCore from 'react-native-fbsdkcore';
 const { FBSDKLoginButton } = FBSDKLogin;
+const { FBSDKAccessToken, FBSDKGraphRequest } = FBSDKCore;
 
-const { AppRegistry, StyleSheet, Text, View } = React;
+const { AppRegistry, StyleSheet, View, Text, TouchableOpacity } = React;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -22,24 +24,70 @@ const styles = StyleSheet.create({
     }
 });
 class starterpack extends React.Component {
+    onLoginFinished(error, result) {
+        if (error) {
+            alert('Error logging in.');
+        } else {
+            if (result.isCancelled) {
+                alert('Login cancelled.');
+            } else {
+                alert('Logged in.');
+            }
+        }
+    }
+
+    onLogoutFinished() {
+        alert('Logged out.');
+    }
+
+    getAccessToken() {
+        FBSDKAccessToken.getCurrentAccessToken((token) => {
+            if (token) {
+                return alert(JSON.stringify(token));
+            }
+            alert('no token');
+        });
+    }
+
+    getInfo() {
+        const fetchFriendsRequest = new FBSDKGraphRequest((error, result) => {
+            if (error) {
+                alert('Error making request.');
+            } else {
+                // Data from request is in result
+                alert(JSON.stringify(result));
+            }
+        }, '/me/');
+        // Start the graph request.
+        fetchFriendsRequest.start();
+    }
+
+    refreshToken() {
+        FBSDKAccessToken.refreshCurrentAccessToken((response) => {
+            alert(JSON.stringify(response));
+        });
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <FBSDKLoginButton
-                         onLoginFinished={(error, result) => {
-                           if (error) {
-                             alert('Error logging in.');
-                           } else {
-                             if (result.isCancelled) {
-                               alert('Login cancelled.');
-                             } else {
-                               alert('Logged in.');
-                             }
-                           }
-                         }}
-                         onLogoutFinished={() => alert('Logged out.')}
-                         readPermissions={[]}
-                         publishPermissions={['publish_actions']}/>
+                    onLoginFinished={this.onLoginFinished}
+                    onLogoutFinished={this.onLogoutFinished}
+                    readPermissions={['public_profile', 'email', 'user_friends']}
+                    publishPermissions={['publish_actions']}/>
+
+                <TouchableOpacity onPress={this.getAccessToken}>
+                    <Text>Get Accesstoken</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={this.refreshToken}>
+                    <Text>Refresh token</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={this.getInfo}>
+                    <Text>Get info</Text>
+                </TouchableOpacity>
             </View>
         );
     }
